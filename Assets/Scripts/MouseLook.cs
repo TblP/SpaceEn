e11e7ceafc28bc1a03d5e6cc7e1d24a5932e3f31
TblP AI <vczyplenkov@mail.ru>
2021-09-06 19:40:47 +0300

    map + move

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform head;
+    private float rotationY;
+    public float sensitivity = 5f; // чувствительность мыши
+    public float headMinY = -40f; // ограничение угла для головы
+    public float headMaxY = 40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX = head.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, headMinY, headMaxY);
+        head.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float jump = 1;
+    public float gravity = 0.01f;
+    public float horizontInput;
+    public float verticalInput;
+    Vector3 move_Direction;
+    CharacterController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        horizontInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        if (player.isGrounded)
+        {
+            move_Direction = new Vector3(horizontInput, 0, verticalInput);
+            move_Direction = transform.TransformDirection(move_Direction);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                move_Direction.y += jump;
+            }
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                player.height = 1.0f;
+            }
+            else {  
+                player.height = 1.7f;
+            }
+            
+
+        }
+        move_Direction.y -= gravity;
+        player.Move(move_Direction * speed * Time.deltaTime);
+    }
+
+}
